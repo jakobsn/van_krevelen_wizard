bcab212e9cfffdfbeb0ae9674ed7e06316c674d8
jakobsn <jakob_sn_@hotmail.com>
2020-05-29 13:57:05 +0200

    forgot to add filter files

---
+++ pages/filter/filter.js
@@ -0,0 +1,58 @@
+var workbook = new ExcelJS.Workbook();
+
+$("#filter-button").on("click", function() {
+    
+})
+
+$("#dragable").on("dragover", function(event) {
+    event.preventDefault();  
+    event.stopPropagation();
+    $(this).addClass('dragging');
+});
+
+$("#dragable").on("dragleave", function(event) {
+    event.preventDefault();  
+    event.stopPropagation();
+    $(this).removeClass('dragging');
+});
+
+$("#dragable").on("drop", function(event) {
+    event.preventDefault();  
+    event.stopPropagation();
+    console.log("Dropped!");
+    console.log(event.originalEvent.dataTransfer.files)
+    for(let i = 0; i < event.originalEvent.dataTransfer.files.length; i++) {
+        workbook.xlsx.readFile(event.originalEvent.dataTransfer.files[i].path).then( (document) => {
+            generateTable(document)
+        })
+        break // Support for several documents not implemented
+    }
+}); 
+
+// TODO: This is vulnerable to injection
+function generateTable(document){
+    let header_list = []
+    let header = document.worksheets[0].getRow(1).values
+    for(let i = 0; i < header.length; i++){
+        header_list.push({"title": header[i]})
+    }
+
+    let table = $('#preview-table').DataTable({
+        "columns": header_list.slice(1)
+    })
+
+    document.worksheets[0].eachRow({ includeEmpty: true }, function(row, rowNumber){
+        if(rowNumber > 1){
+            real_row = []
+            row.eachCell({includeEmpty: true}, function(cell, colNumber){
+                real_row.push(cell.value == null ? "null" : cell.value)
+            })
+            table.row.add(
+                real_row
+            )
+        }
+
+    })
+    table.draw()
+
+}
